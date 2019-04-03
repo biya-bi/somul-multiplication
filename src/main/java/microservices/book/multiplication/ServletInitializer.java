ec5a688623375db189fa11468b0327b0109c2a26
Paul Muriel Biya-Bi <biya-bi@hotmail.fr>
2019-04-02 22:40:50 -0400

    Add a servlet initializer
    We add a servlet initializer just in case we wish to build a war file
    and deploy it. For microservices that are ran only in embedded
    containers, the servlet initializer is not required.

---
+++ src/main/java/microservices/book/multiplication/ServletInitializer.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SocialMultiplicationApplication.class);
+	}
+
+}
