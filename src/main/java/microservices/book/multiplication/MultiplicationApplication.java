d4c894ce16fb5445dd3c19f5c6e836207abc16f4
Paul Muriel Biya-Bi <biya-bi@hotmail.fr>
2019-04-02 23:12:21 -0400

    Rename the application's main class

--- src/main/java/microservices/book/multiplication/MultiplicationApplication.java
+++ src/main/java/microservices/book/multiplication/MultiplicationApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SocialMultiplicationApplication {
+public class MultiplicationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialMultiplicationApplication.class, args);
+		SpringApplication.run(MultiplicationApplication.class, args);
 	}
 
 }
