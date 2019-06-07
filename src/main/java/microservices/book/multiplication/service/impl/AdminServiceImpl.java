a454416e2a58341ce81c7b9da9f62f0fc9175596
Paul Muriel Biya-Bi <biya-bi@hotmail.fr>
2019-06-07 16:04:15 -0400

    Create a test profile

---
+++ src/main/java/microservices/book/multiplication/service/impl/AdminServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package microservices.book.multiplication.service.impl;
+
+import microservices.book.multiplication.repository.MultiplicationRepository;
+import microservices.book.multiplication.repository.MultiplicationResultAttemptRepository;
+import microservices.book.multiplication.repository.UserRepository;
+import microservices.book.multiplication.service.AdminService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author @author biya-bi
+ */
+@Profile("test")
+@Service
+class AdminServiceImpl implements AdminService {
+
+	private MultiplicationRepository multiplicationRepository;
+	private MultiplicationResultAttemptRepository attemptRepository;
+	private UserRepository userRepository;
+
+	@Autowired
+	AdminServiceImpl(final MultiplicationRepository multiplicationRepository, final UserRepository userRepository,
+			final MultiplicationResultAttemptRepository attemptRepository) {
+		this.multiplicationRepository = multiplicationRepository;
+		this.userRepository = userRepository;
+		this.attemptRepository = attemptRepository;
+	}
+
+	@Override
+	public void deleteDatabaseContents() {
+		attemptRepository.deleteAll();
+		multiplicationRepository.deleteAll();
+		userRepository.deleteAll();
+	}
+}
